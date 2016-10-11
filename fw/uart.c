3d6d9980a601c2499a8dcf85207de4850600324f
Matthias Hannig <matthias@hannig.cc>
2016-10-11 10:07:29 +0200

    added uart read code

--- fw/uart.c
+++ fw/uart.c
@@ -2,6 +2,45 @@
 #include "uart.h"
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+
+
+volatile uint8_t _uart_string_ready = 0;
+volatile uint8_t _uart_string_length = 0;
+volatile char    _uart_string_buffer[UART_BUFFER_SIZE] = "";
+
+/**
+ * UART receive interrupt
+ */
+ISR( USART_RX_vect )
+{
+    static uint8_t count;
+    unsigned char c = UDR0;
+
+    if( _uart_string_ready == 0 ) {
+      if( c == '\r' ) {
+      }
+      else if( c != '\n' &&
+          count < UART_BUFFER_SIZE - 1 ) {
+          // append char to string
+          _uart_string_buffer[count] = c;
+          count++;
+      }
+      else {
+          // terminate string
+          _uart_string_buffer[count] = '\0';
+          _uart_string_length = count;
+          count = 0;
+          _uart_string_ready = 1;
+      }
+    }
+
+  // echo back
+  #if UART_ECHO == 1
+      UDR0 = c;
+  #endif
+}
 
 /**
  * Transmit a single char
@@ -37,6 +76,20 @@ void USART_writeln(const char *s)
     USART_puts(s);
     USART_tx('\n');
     USART_tx('\r');
+}
+
+/**
+ * Wait until string ready,
+ * return pointer to string buffer
+ */
+char* USART_read()
+{
+	if( _uart_string_ready == 1 ) {
+		_uart_string_ready = 0;
+		return (char*)_uart_string_buffer;
+	}
+
+	return NULL;
 }
 
 /**
